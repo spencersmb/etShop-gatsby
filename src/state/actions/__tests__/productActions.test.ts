e31289ddfeca4cd2872425021ef32bdf766f12d2
Spencer Bigum <spencer.bigum@gmail.com>
2019-02-17 21:28:50 -0500

    added product action test

---
+++ src/state/actions/__tests__/productActions.test.ts
@@ -0,0 +1,31 @@
+/* eslint-disable no-unused-vars */
+import { AuthActionTypes, ProductsActionTypes } from '@et/types/Enums'
+import { login, logout } from '@redux/actions/authActions'
+import { loadProducts } from '@redux/actions/productActions'
+import data from '@redux/products.json'
+import { cleanup } from 'react-testing-library'
+import configureMockStore from 'redux-mock-store'
+import thunk from 'redux-thunk'
+import initialState from '@redux/reducers/initialState'
+
+const middlewares = [thunk]
+const mockStore = configureMockStore(middlewares)
+afterEach(cleanup)
+describe('Product Action tests', () => {
+
+	it('Should have type LOAD_PRODUCTS_SUCCESS', () => {
+
+		const store = mockStore(initialState)
+		store.dispatch(loadProducts())
+		const getActions = store.getActions()
+		const expectedActions =
+			{
+				type: ProductsActionTypes.LOAD_PRODUCTS_SUCCESS,
+				payload: data
+			}
+
+		expect(getActions.length).toBe(1)
+		expect(getActions).toEqual([expectedActions])
+	})
+
+})
--- src/state/actions/productActions.ts
+++ src/state/actions/productActions.ts
@@ -1,23 +1,10 @@
 import { Actions } from '@et/types/Actions'
 import { ProductsActionTypes } from '@et/types/Enums'
-// import {Action, Dispatch} from 'redux'
 import data from '../products.json'
 
-export const count = (): Actions => {
-  return {
-    type: ProductsActionTypes.COUNT
-  }
-}
-
 export const loadProducts = (): Actions => {
-  if(!data){
-    return {
-      type: ProductsActionTypes.LOAD_PRODUCTS_ERROR
-    }
-  }
-
-  return {
-    type: ProductsActionTypes.LOAD_PRODUCTS_SUCCESS,
-    payload: data
-  }
+	return {
+		type: ProductsActionTypes.LOAD_PRODUCTS_SUCCESS,
+		payload: data
+	}
 }
